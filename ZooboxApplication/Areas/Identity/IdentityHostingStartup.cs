928d3107f559936e2acaf16facd1b69cae426dc6
Diogo Paulino <diogopaulino@mail.com>
2018-11-24 14:40:48 +0000

    first commit with authentication

---
+++ ZooboxApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZooboxApplication.Data;
+
+[assembly: HostingStartup(typeof(ZooboxApplication.Areas.Identity.IdentityHostingStartup))]
+namespace ZooboxApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
