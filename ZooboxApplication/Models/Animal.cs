b03addde4bd0d47a9fc3f254520ebabc17165e71
André Silva GMV <asilva@gmv.com>
2018-12-06 22:18:50 +0000

    Animal Crud

---
+++ ZooboxApplication/Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZooboxApplication.Models
+{
+    public class Animal 
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+
+
+     
+    }
+}
